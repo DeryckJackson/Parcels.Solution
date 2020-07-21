0b74dbf68c8b377457f7b9f137793d4a937c90dc
Ian Scott <chesnekov@gmail.com>
2020-07-21 10:50:48 -0700

    initial dev environment. UPdates names references to reflect project name, updates .csproj files which were lacking the correct Project sdk reference
    Co-authored-by: Deryck Jackson <Dgjackson01@gmail.com>

--- Parcels/Controllers/HomeControllers.cs
+++ Parcels/Controllers/HomeControllers.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
-using FriendLetter.Models;
+using Parcels.Models;
 
-namespace FriendLetter.Controllers
+namespace Parcels.Controllers
 {
   public class HomeController : Controller
   {
